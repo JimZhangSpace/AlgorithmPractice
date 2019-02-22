d0f9e89d4fbaec1491222a5b5f250925223244cb
JimZhang <zj_potato@163.com>
2019-02-22 14:21:37 +0800

    add findmedian algorithm

---
+++ app/src/main/java/com/zj/algorithmdemo/FindMedian.kt
@@ -0,0 +1,27 @@
+package com.zj.algorithmdemo
+
+class FindMedian {
+
+    companion object {
+
+        @JvmStatic fun main(args: Array<String>){
+            println("Hello,world!")
+
+
+        }
+    }
+
+    fun findMedianFromSortedArrays(array1:IntArray,array2:IntArray):Double {
+
+        var median:Double = 0.0
+        var newArray = array1.copyOf(array1.size+array2.size)
+
+
+        
+
+
+        return median
+
+    }
+
+}
